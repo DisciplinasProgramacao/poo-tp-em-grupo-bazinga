75f7438997fadf706e708696b0f8a7f926f9b07a
Caio Luz <henriquecaio3329@gmail.com>
2023-10-25 19:28:13 -0300

    Adicionando métodos faltantes

--- codigo/src/Frota.java
+++ codigo/src/Frota.java
@@ -54,6 +54,82 @@ public class Frota {
         return kmTotalFrota;
     }
 
+
+    /**
+     * Compara as quilometragens totais {@code double} 
+     * dos veículos que compõem esta frota e fornece qual deles tem a maior quilometragem total {@code double}.
+     *
+     * @return  {@code Veiculo} Veiculo com maior quilometragem total.
+     *
+     */
+    public Veiculo maiorKmTotal(){
+        Veiculo maiorKm = new Veiculo();
+        for(Veiculo veiculo : this.veiculos){
+            if(maiorKm.kmTotal() < veiculo.kmTotal()){
+                maiorKm = veiculo;
+            }
+           
+        }
+        return maiorKm;
+    }
+
+
+    /**
+     * NÃO ENTENDI
+     * Compara as quilometragens totais {@code double} 
+     * dos veículos que compõem esta frota e fornece qual deles tem a maior quilometragem total {@code double}.
+     *
+     * @return  {@code Veiculo} Veiculo com maior quilometragem total.
+     *
+     */
+    public Veiculo maiorKmMedia(){
+        Veiculo x = new Veiculo();
+        return x; // Peruntar antes de finalizar
+    }
+
+
+    /**
+     * Fornece um relatório com os dados da frota {@code String} e
+     * dos veículos que compõem esta frota.
+     * 
+     * <p> Dados da frota:
+     * <ul>
+     *   <li> Tamanho
+     *   <li> Dados dos Veículos
+     *     <ul>
+     *       <li> Placa
+     *       <li> Km Total   
+     *     </ul>
+     * </ul>
+     *
+     * @return  {@code String} relatório com os dados da frota.
+     *
+     */
+    public String relatorioFrota(){
+        StringBuilder relatorio = new StringBuilder();
+
+        relatorio.append("=================\nRELATÓRIO\n=================\n");
+        relatorio.append("TAMANHO DA FROTA: " + this.tamanhoFrota + "veículos\n");
+        relatorio.append("=================\nDADOS DOS VEÍCULOS\n=================\n");
+
+        for(Veiculo veiculo : this.veiculos){
+            relatorio.append("PLACA: " + veiculo.getPlaca() + "\n");
+            relatorio.append("KM TOTAL: " + veiculo.kmTotal() + "KM\n");
+            relatorio.append("=================\n");
+        }
+
+        return relatorio.toString();
+    }
+
+
+
+
+
+
+
+
+
+
     
 
 
